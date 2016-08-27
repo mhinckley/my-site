19c0435b08ecca649b7157ce62950c8766bf8cf0
Michael Hinckley <michael.a.hinckley@gmail.com>
2016-08-27 14:48:41 -0700

    Tweaked formatting of homepage

--- board/views.py
+++ board/views.py
@@ -13,7 +13,7 @@ from django.core.urlresolvers import reverse
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
-    paginator = Paginator(posts, 2)
+    paginator = Paginator(posts, 5)
     page = request.GET.get('page')
     try:
         posts = paginator.page(page)
